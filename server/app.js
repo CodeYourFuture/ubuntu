409bc2ca417ccfb38ad5e62a3ed68144e03c4d59
YJohn <yfekadu2016@gmail.com>
2018-08-07 12:03:50 +0100

    Add handlebars for server-side

--- server/app.js
+++ server/app.js
@@ -1,8 +1,25 @@
 const SERVER_PORT = process.env.PORT || 3000;
 const express = require("express");
 const app = express();
+const exphbs = require("express-handlebars");
 
-app.get("/", (req, res) => res.send("Hello world"));
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs"
+  })
+);
+
+app.set("view engine", "hbs");
+
+app.get("/", (req, res) => res.render("home"));
+
+app.get("/admin/referrals", (req, res) => res.render("referrals"));
+app.get("/admin/shelters", (req, res) => res.render("shelters"));
+
+app;
+
 app.get("/api/status", (req, res) => {
   res.send({
     status: OK
