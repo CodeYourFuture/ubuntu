cd63cb0fb9adc25522aba6a129c15e7b54d7ab3b
YJohn <yfekadu2016@gmail.com>
2018-08-07 12:08:48 +0100

    use express.Router

--- server/app.js
+++ server/app.js
@@ -1,7 +1,9 @@
-const SERVER_PORT = process.env.PORT || 3000;
+const SERVER_PORT = process.env.PORT || 4000;
 const express = require("express");
 const app = express();
 const exphbs = require("express-handlebars");
+const api = require("./api");
+const admin = require("./admin");
 
 app.engine(
   "hbs",
@@ -15,17 +17,9 @@ app.set("view engine", "hbs");
 
 app.get("/", (req, res) => res.render("home"));
 
-app.get("/admin/referrals", (req, res) => res.render("referrals"));
-app.get("/admin/shelters", (req, res) => res.render("shelters"));
-
-app;
-
-app.get("/api/status", (req, res) => {
-  res.send({
-    status: OK
-  });
-});
+app.use("/admin", admin);
+app.use("/api", api);
 
 app.listen(SERVER_PORT, () =>
-  console.log(`Ubuntu server running on ${SERVER_PORT}`)
+  console.log(`Ubuntu server running on http://localhost:${SERVER_PORT}`)
 );
