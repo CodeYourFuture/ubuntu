ba864e11344e2bf3fc9d4a09f743f483824dd610
Soha <s.azadi12319@gmail.com>
2018-08-10 21:24:18 +0100

    volunteers-list

--- server/seeds/volunteers-list.js
+++ server/seeds/volunteers-list.js
@@ -1,16 +1,15 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex("volunteers-list")
+  return knex("volunteers")
     .del()
     .then(function() {
       // Inserts seed entries
-      return knex("volunteers-list").insert([
+      return knex("volunteers").insert([
         {
           name: "Ubuntu volunteers"
         },
         {
-          name: "soheila",
-          role: "cashier"
+          name: "soheila"
         }
       ]);
     });
