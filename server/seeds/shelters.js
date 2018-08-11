472c63f3f5330d3c426c6dba599a2f4ff2b1ff64
Soha <s.azadi12319@gmail.com>
2018-08-11 17:03:17 +0100

    Redo schema from scratch
    Deleted individual migration files in favour of defining the whole
    schema in a single migration (called "base-schema") which is
    easier to read.
    
    The seed files no longer match the current schema so have been
    removed. A future commit will add new seed data in a single file,
    providing consistend relation ids (since we can't tell which order
    knex will run different files).

--- server/seeds/shelters.js
+++ server/seeds/shelters.js
@@ -10,7 +10,7 @@ exports.seed = function(knex, Promise) {
         },
         {
           name: "Edinburgh men shelter",
-          city: "Edinburgh"
+          address: "Edinburgh"
         }
       ]);
     });
