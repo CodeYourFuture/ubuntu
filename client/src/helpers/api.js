94c65cbe868aff110418f5a4321eb52dff971224
LslyKng <prezzles@gmail.com>
2018-08-16 16:19:02 +0100

    Deleted extra getAddresses function in React client
    An extra api helper function was accidentally committed in previous
    change.

--- client/src/helpers/api.js
+++ client/src/helpers/api.js
@@ -11,15 +11,11 @@ export const getShelters = () => {
 export const getAddresses = () => {
   return fetch(`${API_URL}/api/addresses`).then(res => res.json());
 };
+
 export const getReferrals = () => {
   return fetch(`${API_URL}/api/referrals`).then(res => res.json());
-};
-
-export const getAddresses = () => {
-  return fetch(`${API_URL}/api/address`).then(res => res.json());
 };
 
 export const getUsers = () => {
   return fetch(`${API_URL}/api/users`).then(res => res.json());
 };
-
