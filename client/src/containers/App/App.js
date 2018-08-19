5e5372bb34631aeb08550947ee376bc8166cc5fb
YJohn <yfekadu2016@gmail.com>
2018-08-19 17:39:22 +0100

    protect app with login

--- client/src/containers/App/App.js
+++ client/src/containers/App/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import Home from "../Home/Home";
 import About from "../About/About";
@@ -7,41 +7,72 @@ import Header from "../../components/Header/Header";
 import Shelters from "../Shelters/Shelters";
 import Referrals from "../Referrals/Referrals";
 import Users from "../Users/Users";
+import axios from "axios";
 
-const App = () => (
-  <Router>
-    <div className="app">
-      <Header />
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-        <li>
-          <Link to="/contactUs">Contact Us</Link>
-        </li>
-        <li>
-          <Link to="/shelters">Shelters</Link>
-        </li>
-        <li>
-          <Link to="/referrals">Referrals</Link>
-        </li>
-        <li>
-          <Link to="/users">Users</Link>
-        </li>
-      </ul>
-      <hr />
+class App extends Component {
+  state = {
+    loggedIn: false
+  };
+  componentDidMount = async () => {
+    const token = localStorage.getItem("jwtToken");
+    axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
 
-      <Route exact path="/" component={Home} />
-      <Route path="/about" component={About} />
-      <Route path="/contactUs" component={ContactUs} />
-      <Route path="/shelters" component={Shelters} />
-      <Route path="/referrals" component={Referrals} />
-      <Route path="/users" component={Users} />
-    </div>
-  </Router>
-);
+    if (!token) {
+      return this.props.history.push("/login");
+    }
+  };
+
+  logout = () => {
+    localStorage.removeItem("jwtToken");
+    window.location.reload();
+  };
+
+  render() {
+    const token = localStorage.getItem("jwtToken");
+    if (!token) return null;
+    return (
+      <Router>
+        <div className="app">
+          <Header />
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contactUs">Contact Us</Link>
+            </li>
+            <li>
+              <Link to="/shelters">Shelters</Link>
+            </li>
+            <li>
+              <Link to="/referrals">Referrals</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+            <li>
+              {localStorage.getItem("jwtToken") && (
+                <button className="btn btn-primary" onClick={this.logout}>
+                  Logout
+                </button>
+              )}
+            </li>
+          </ul>
+          <hr />
+
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/contactUs" component={ContactUs} />
+          <Route path="/shelters" component={Shelters} />
+          <Route path="/referrals" component={Referrals} />
+          <Route path="/users" component={Users} />
+        </div>
+      </Router>
+    );
+  }
+}
 
 export default App;
