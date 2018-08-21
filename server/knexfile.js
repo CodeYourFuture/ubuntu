ee00aeb8036570ab2da634b4b2b64a7af2342c26
Rares Matei <r.matei.10@aberdeen.ac.uk>
2018-08-21 22:11:33 +0100

    add heroku integration

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 module.exports = {
   development: {
-    client: "postgresql",
+    client: "pg",
     connection: {
       host: process.env.DB_HOST || "127.0.0.1",
       database: process.env.DB_NAME || "ubuntu",
@@ -17,31 +17,9 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
-
   production: {
-    client: "postgresql",
-    connection: {
-      host: process.env.DB_HOST,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
-    },
+    client: "ps",
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
