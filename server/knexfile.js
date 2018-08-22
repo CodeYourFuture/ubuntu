fc1607bdd5290b6d97bce973ec959e4254be7c51
Rares Matei <r.matei.10@aberdeen.ac.uk>
2018-08-22 21:53:58 +0100

    PR feedback

--- server/knexfile.js
+++ server/knexfile.js
@@ -3,10 +3,10 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
-      host: process.env.DB_HOST || "127.0.0.1",
-      database: process.env.DB_NAME || "ubuntu",
-      user: process.env.DB_USER || "cyf",
-      password: process.env.DB_PASSWORD || "password"
+      host: "127.0.0.1",
+      database: "ubuntu",
+      user: "cyf",
+      password: "password"
     },
     pool: {
       min: 2,
@@ -18,7 +18,7 @@ module.exports = {
   },
 
   production: {
-    client: "ps",
+    client: "pg",
     connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
