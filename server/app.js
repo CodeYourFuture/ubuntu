8b0cfb18b4fed94f6b8bedac5bff57f45544c025
Mozafar <kabaros@gmail.com>
2018-09-01 14:39:27 +0100

    Fix server going into non-development mode

--- server/app.js
+++ server/app.js
@@ -1,31 +1,31 @@
-const SERVER_PORT = process.env.PORT || 4000;
-const express = require("express");
-const app = express();
-const api = require("./api");
-const cors = require("cors");
-const user = require("./routes/user");
-const auth = require("./routes/auth");
-const passport = require("passport");
-const path = require("path");
-const bodyParser = require("body-parser");
-require("./passport");
+const SERVER_PORT = process.env.PORT || 4000
+const express = require('express')
+const app = express()
+const api = require('./api')
+const cors = require('cors')
+const user = require('./routes/user')
+const auth = require('./routes/auth')
+const passport = require('passport')
+const path = require('path')
+const bodyParser = require('body-parser')
+require('./passport')
 
-app.use(cors());
-app.use(bodyParser());
+app.use(cors())
+app.use(bodyParser())
 
-app.use("/api", api);
-app.use("/auth", auth);
-app.use("/user", passport.authenticate("jwt", { session: false }), user);
+app.use('/api', api)
+app.use('/auth', auth)
+app.use('/user', passport.authenticate('jwt', { session: false }), user)
 
 // In development environemnt, we use the create-react-app dev server
 // In production, the static build is served from here
-if (process.env.NODE_ENV !== "development") {
-  app.use("/", express.static(path.resolve(__dirname, "../client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
-  });
+if (process.env.NODE_ENV && process.env.NODE_ENV !== 'development') {
+  app.use('/', express.static(path.resolve(__dirname, '../client/build')))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
+  })
 }
 
 app.listen(SERVER_PORT, () =>
   console.log(`Ubuntu server running on http://localhost:${SERVER_PORT}`)
-);
+)
--- server/helpers/db.js
+++ server/helpers/db.js
@@ -1,50 +1,46 @@
-const config = require("../knexfile")[process.env.NODE_ENV || "development"];
+const config = require('../knexfile')[process.env.NODE_ENV || 'development']
 
-const knex = require("knex")(config);
+const knex = require('knex')(config)
 
 const getShelters = () => {
-  return knex.select().from("shelters");
-};
-
+  return knex.select().from('shelters')
+}
 
 const getOrganisations = () => {
-  return knex.select().from("organisations");
-};
+  return knex.select().from('organisations')
+}
 
 const getAddresses = () => {
-  return knex.select().from("addresses");
-};
+  return knex.select().from('addresses')
+}
 
 const getReferrals = () => {
-  return knex.select().from("referrals");
-};
+  return knex.select().from('referrals')
+}
 
 const saveOrganisation = organisation => {
-  return knex("organisations").insert(organisation);
-};
+  return knex('organisations').insert(organisation)
+}
 
 const getShelterAbout = () => {
-  return knex.select().from("about");
-};
+  return knex.select().from('about')
+}
 
 const getUsers = () => {
-  return knex.select().from("users");
-};
+  return knex.select().from('users')
+}
 
 const getSingleUser = (email, password) => {
-  return knex("users")
-    .where({ email, password })
-    .first();
-};
+  return knex('users').where({ email, password }).first()
+}
 const getUserProfile = userId => {
-  return knex("users")
-    .select("user_id", "email", "name")
+  return knex('users')
+    .select('user_id', 'email', 'name')
     .where({ user_id: userId })
-    .first();
-};
+    .first()
+}
 
 module.exports = {
-  getStatus: getStatus,
   getShelters: getShelters,
   getOrganisations: getOrganisations,
   getAddresses: getAddresses,
@@ -56,4 +52,4 @@ module.exports = {
   getUserProfile,
   saveOrganisation,
   getShelterAbout
-};
+}
