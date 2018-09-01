f4d18cceaea3df1c347b3e50ad3b6542ea7880a7
Mozafar <kabaros@gmail.com>
2018-09-01 14:34:37 +0100

    Fix issues with database schema

--- server/seeds/init_db.js
+++ server/seeds/init_db.js
@@ -1,38 +1,48 @@
 exports.seed = async (knex, Promise) => {
   // Deletes ALL existing entries
-  await knex("referrals").del();
-  await knex("shelters").del();
+  await knex('referrals').del()
+  await knex('shelters').del()
 
-  const shelters = await knex("shelters")
-    .returning("shelter_id")
-    .insert([
-      {
-        name: "Ubuntu Women Shelter",
-        address: "G4",
-        details: "very big shelter...."
-      },
-      {
-        name: "Edinburgh Men Shelter",
-        address: "E1",
-        details: "men shelter...."
-      }
-    ]);
+  const shelters = await knex('shelters').returning('shelter_id').insert([
+    {
+      name: 'Ubuntu Women Shelter',
+      address: 'G4',
+      details: 'very big shelter....'
+    },
+    {
+      name: 'Edinburgh Men Shelter',
+      address: 'E1',
+      details: 'men shelter....'
+    }
+  ])
 
-  const ubuntuShelterId = shelters[0];
-  const edinburghShelterId = shelters[1];
+  const ubuntuShelterId = shelters[0]
+  const edinburghShelterId = shelters[1]
 
   // Inserts seed entries
-  await knex("referrals").insert([
-    { name: "Mozafar", shelter_id: edinburghShelterId },
-    { name: "Mimi", shelter_id: ubuntuShelterId },
-    { name: "Etza", shelter_id: ubuntuShelterId }
-  ]);
+  await knex('referrals').insert([
+    { name: 'Mozafar', shelter_id: edinburghShelterId },
+    { name: 'Mimi', shelter_id: ubuntuShelterId },
+    { name: 'Etza', shelter_id: ubuntuShelterId }
+  ])
 
-  await knex("users").insert([
+  await knex('users').insert([
     {
-      name: "yohannes",
-      email: "email@email.com",
-      password: "password"
+      name: 'yohannes',
+      email: 'email@email.com',
+      password: 'password'
     }
-  ]);
-};
+  ])
+
+  await knex('volunteers').del()
+
+  // Inserts seed entries
+  return knex('volunteers').insert([
+    {
+      name: 'Ubuntu volunteers'
+    },
+    {
+      name: 'soheila'
+    }
+  ])
+}
