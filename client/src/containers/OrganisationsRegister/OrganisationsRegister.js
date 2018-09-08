b1c1727730302169d1e485041c0ea4b35d85864a
LslyKng <prezzles@gmail.com>
2018-09-08 14:33:13 +0100

    Register form with bootstrap

--- client/src/containers/OrganisationsRegister/OrganisationsRegister.js
+++ client/src/containers/OrganisationsRegister/OrganisationsRegister.js
@@ -17,48 +17,46 @@ class OrganisationsRegister extends React.Component {
   };
   render() {
     return (
-      
-      // WRONG BOOTSTRAP VERSION
-//       <form onSubmit={this.onSubmit} className="organisation-details">
-//         <p className="h4 text-center mb-4">Register your organisation</p>
-//         <label htmlFor="organisationName" className="grey-text">
-//           Organisation name
-//         </label>
-//         <input type="text" id="organisationName" className="form-control" />
-//         <br />
-//         <label htmlFor="Name" className="grey-text">
-//           Your name
-//         </label>
-//         <input type="text" id="yourName" className="form-control" />
-//         <br />
-//         <label htmlFor="address" className="grey-text">
-//           Your address
-//         </label>
-//         <input type="text" id="address" className="form-control" />
-//         <br />
-//         <label htmlFor="email" className="grey-text">
-//           Email
-//         </label>
-//         <input type="email" id="email" className="form-control" />
-//         <br />
-//         <label htmlFor="phoneNumber" className="grey-text">
-//           Phone number
-//         </label>
-//         <input type="number" id="number" className="form-control" />
-//         <br />
-//         <label htmlFor="password" className="grey-text">
-//           Password
-//         </label>
-//         <input type="password" id="password" className="form-control" />
-//         <div className="text-center mt-4">
-//           <button className="btn btn-unique" type="submit">
-//             Register
-//           </button>
-//         </div>
-//       </form>
-//     );
-//   }
-// }
+      <form onSubmit={this.onSubmit} className="organisation-details">
+        <p className="h4 text-center mb-4">Register your organisation</p>
+        <label htmlFor="organisationName" className="grey-text">
+          Organisation name
+        </label>
+        <input type="text" id="organisationName" className="form-control" />
+        <br />
+        <label htmlFor="Name" className="grey-text">
+          Your name
+        </label>
+        <input type="text" id="yourName" className="form-control" />
+        <br />
+        <label htmlFor="address" className="grey-text">
+          Your address
+        </label>
+        <input type="text" id="address" className="form-control" />
+        <br />
+        <label htmlFor="email" className="grey-text">
+          Email
+        </label>
+        <input type="email" id="email" className="form-control" />
+        <br />
+        <label htmlFor="phoneNumber" className="grey-text">
+          Phone number
+        </label>
+        <input type="number" id="number" className="form-control" />
+        <br />
+        <label htmlFor="password" className="grey-text">
+          Password
+        </label>
+        <input type="password" id="password" className="form-control" />
+        <div className="text-center mt-4">
+          <button className="btn btn-unique" type="submit">
+            Register
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
 export default OrganisationsRegister;
 
 /* <h1 className="heading">Register your organisation</h1>
