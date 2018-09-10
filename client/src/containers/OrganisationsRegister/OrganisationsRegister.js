07dd3497c8071b1aef29a401d2ded1e9a68ba504
LslyKng <35503136+LslyKng@users.noreply.github.com>
2018-09-10 16:13:17 +0100

    Register organisations 3 (#41)
    * submit button changed to allow on enter
    
    * changing the api name
    
    * random files on my computer that are not mine
    
    * bootstrap form added
    
    * Register form with bootstrap
    
    * bootstrap form
    
    * bootstrap form nothing commented out
    
    * nooooooo
    
    * css added for register organistion form
    
    * ongoing changes to regOrg form for validation and to make it similar to login form
    
    * Css changed to reflect bootstrap changes in js file
    
    * completed form with redirect to login when complete

--- client/src/containers/OrganisationsRegister/OrganisationsRegister.js
+++ client/src/containers/OrganisationsRegister/OrganisationsRegister.js
@@ -8,52 +8,147 @@ class OrganisationsRegister extends React.Component {
     address: "",
     email: "",
     phoneNumber: "",
-    password: ""
+    password: "",
+    formValid: true
   };
+
+  // componentDidMount() {
+  //   api.getOrganisationsRegister().then(data => {
+  //     this.setState({
+  //       users: data
+  //     });
+  //   });
+  // }
 
   onSubmit = e => {
     e.preventDefault();
-    api.saveUser(this.state);
+    console.log(this.state);
+    if (this.state.contactName.length === 0) {
+      // invalid
+      this.setState({
+        formValid: false
+      });
+    } else {
+      //valid
+      this.setState({ formValid: true });
+      api.saveUser(this.state).then(() => {
+        this.props.history.push("/login");
+      });
+    }
   };
+
+  onChange = e => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
   render() {
     return (
-      <form onSubmit={this.onSubmit} className="organisation-details">
-        <p className="h4 text-center mb-4">Register your organisation</p>
-        <label htmlFor="organisationName" className="grey-text">
-          Organisation name
-        </label>
-        <input type="text" id="organisationName" className="form-control " />
-        <br />
-        <label htmlFor="Name" className="grey-text">
-          Your name
-        </label>
-        <input type="text" id="yourName" className="form-control" />
-        <br />
-        <label htmlFor="address" className="grey-text">
-          Your address
-        </label>
-        <input type="text" id="address" className="form-control" />
-        <br />
-        <label htmlFor="email" className="grey-text">
-          Email
-        </label>
-        <input type="email" id="email" className="form-control" />
-        <br />
-        <label htmlFor="phoneNumber" className="grey-text">
-          Phone number
-        </label>
-        <input type="number" id="number" className="form-control" />
-        <br />
-        <label htmlFor="password" className="grey-text">
-          Password
-        </label>
-        <input type="password" id="password" className="form-control" />
-        <div className="text-center mt-4">
-          <button className="btn btn-unique" type="submit">
-            Register
-          </button>
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 offset-md-3">
+            <form onSubmit={this.onSubmit}>
+              <p className="h4 text-center mb-4">
+                Please provide all the information requested. Your registration
+                cannot be completed without it.
+              </p>
+              <div className="form-group">
+                <label htmlFor="contactName" className="sr-only">
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  name="contactName"
+                  onChange={this.onChange}
+                  id="contactName"
+                  placeholder="Your name"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="organisationName" className="sr-only" />
+                <input
+                  type="text "
+                  id="organisationName"
+                  name="organisationName"
+                  onChange={this.onChange}
+                  placeholder="Organisation name"
+                  className="form-control"
+                  required
+                />
+                <div class="invalid-feedback">
+                  Please provide your organisation name
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="address" className="sr-only">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  name="address"
+                  onChange={this.onChange}
+                  placeholder="Address"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={this.onChange}
+                  placeholder="Email"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phoneNumber" className="sr-only">
+                  Phone number
+                </label>
+                <input
+                  type="number"
+                  id="number"
+                  name="phoneNumber"
+                  onChange={this.onChange}
+                  placeholder="Phone number"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={this.onChange}
+                  placeholder="Password"
+                  className="form-control"
+                />
+              </div>
+              {!this.state.formValid ? (
+                <div class="alert alert-danger" role="alert">
+                  Form is not valid
+                </div>
+              ) : null}
+              <div className="text-center mt-4">
+                <button className="btn btn-large btn-primary" type="submit">
+                  Register
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </form>
+      </div>
     );
   }
 }
