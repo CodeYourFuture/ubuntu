150f35e8452c37e926c00ced62da30c11ac4023b
LslyKng <prezzles@gmail.com>
2018-09-10 15:33:56 +0100

    completed form with redirect to login when complete

--- client/src/containers/OrganisationsRegister/OrganisationsRegister.js
+++ client/src/containers/OrganisationsRegister/OrganisationsRegister.js
@@ -8,94 +8,146 @@ class OrganisationsRegister extends React.Component {
     address: "",
     email: "",
     phoneNumber: "",
-    password: ""
+    password: "",
+    formValid: true
   };
+
+  // componentDidMount() {
+  //   api.getOrganisationsRegister().then(data => {
+  //     this.setState({
+  //       users: data
+  //     });
+  //   });
+  // }
 
   onSubmit = e => {
     e.preventDefault();
-    api.saveUser(this.state);
+    console.log(this.state);
+    if (this.state.contactName.length === 0) {
+      // invalid
+      this.setState({
+        formValid: false
+      });
+    } else {
+      //valid
+      this.setState({ formValid: true });
+      api.saveUser(this.state).then(() => {
+        this.props.history.push("/login");
+      });
+    }
   };
+
+  onChange = e => {
+    const state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
   render() {
     return (
-      <div className="container form-group">
-        <form
-          className="needs-validation organisation-details"
-          onSubmit={this.onSubmit}
-        >
-          <p className="h4 text-center mb-4">
-            Please provide all the information requested. Your registration
-            cannot be completed without it.
-          </p>
-          <label htmlFor="formCompletionInformation" />
-
-          <div>
-            <label htmlFor="Name required" />
-            <input
-              type="text required value"
-              id="yourName"
-              placeholder="Your name"
-              className="form-control col-center"
-            />
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-6 offset-md-3">
+            <form onSubmit={this.onSubmit}>
+              <p className="h4 text-center mb-4">
+                Please provide all the information requested. Your registration
+                cannot be completed without it.
+              </p>
+              <div className="form-group">
+                <label htmlFor="contactName" className="sr-only">
+                  Your name
+                </label>
+                <input
+                  type="text"
+                  name="contactName"
+                  onChange={this.onChange}
+                  id="contactName"
+                  placeholder="Your name"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="organisationName" className="sr-only" />
+                <input
+                  type="text "
+                  id="organisationName"
+                  name="organisationName"
+                  onChange={this.onChange}
+                  placeholder="Organisation name"
+                  className="form-control"
+                  required
+                />
+                <div class="invalid-feedback">
+                  Please provide your organisation name
+                </div>
+              </div>
+              <div className="form-group">
+                <label htmlFor="address" className="sr-only">
+                  Address
+                </label>
+                <input
+                  type="text"
+                  id="address"
+                  name="address"
+                  onChange={this.onChange}
+                  placeholder="Address"
+                  className="form-control"
+                  required
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  onChange={this.onChange}
+                  placeholder="Email"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="phoneNumber" className="sr-only">
+                  Phone number
+                </label>
+                <input
+                  type="number"
+                  id="number"
+                  name="phoneNumber"
+                  onChange={this.onChange}
+                  placeholder="Phone number"
+                  className="form-control"
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  name="password"
+                  onChange={this.onChange}
+                  placeholder="Password"
+                  className="form-control"
+                />
+              </div>
+              {!this.state.formValid ? (
+                <div class="alert alert-danger" role="alert">
+                  Form is not valid
+                </div>
+              ) : null}
+              <div className="text-center mt-4">
+                <button className="btn btn-large btn-primary" type="submit">
+                  Register
+                </button>
+              </div>
+            </form>
           </div>
-
-          <div>
-            <label htmlFor="organisationName required" />
-            <input
-              type="text "
-              id="organisationName"
-              placeholder="Organisation name"
-              className="form-control col-center "
-              required
-            />
-            <div class="invalid-feedback">
-              Please provide your organisation name
-            </div>
-          </div>
-
-          <div>
-            <label htmlFor="address required" />
-            <input
-              type="text"
-              id="address"
-              placeholder="Address"
-              className="form-control col-center"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="email required" />
-            <input
-              type="email"
-              id="email"
-              placeholder="Email"
-              className="form-control col-center"
-            />
-          </div>
-          <div>
-            <label htmlFor="phoneNumber required" />
-            <input
-              type="number"
-              id="number"
-              placeholder="Phone number"
-              className="form-control col-center"
-            />
-          </div>
-
-          <div>
-            <label htmlFor="password required" />
-            <input
-              type="password"
-              id="password"
-              placeholder="Password"
-              className="form-control col-center "
-            />
-          </div>
-          <div className="text-center mt-4">
-            <button className="btn btn-large btn-primary" type="submit">
-              Register
-            </button>
-          </div>
-        </form>
+        </div>
       </div>
     );
   }
