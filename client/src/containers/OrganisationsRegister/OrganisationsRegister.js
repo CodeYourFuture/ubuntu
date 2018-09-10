a98b2ac0d118cc87b5a4529ed20d47fe7937e93a
LslyKng <prezzles@gmail.com>
2018-09-10 13:12:23 +0100

    ongoing changes to regOrg form for validation and to make it similar to login form

--- client/src/containers/OrganisationsRegister/OrganisationsRegister.js
+++ client/src/containers/OrganisationsRegister/OrganisationsRegister.js
@@ -17,43 +17,86 @@ class OrganisationsRegister extends React.Component {
   };
   render() {
     return (
-      <form onSubmit={this.onSubmit} className="organisation-details">
-        <p className="h4 text-center mb-4">Register your organisation</p>
-        <label htmlFor="organisationName" className="grey-text">
-          Organisation name
-        </label>
-        <input type="text" id="organisationName" className="form-control " />
-        <br />
-        <label htmlFor="Name" className="grey-text">
-          Your name
-        </label>
-        <input type="text" id="yourName" className="form-control" />
-        <br />
-        <label htmlFor="address" className="grey-text">
-          Your address
-        </label>
-        <input type="text" id="address" className="form-control" />
-        <br />
-        <label htmlFor="email" className="grey-text">
-          Email
-        </label>
-        <input type="email" id="email" className="form-control" />
-        <br />
-        <label htmlFor="phoneNumber" className="grey-text">
-          Phone number
-        </label>
-        <input type="number" id="number" className="form-control" />
-        <br />
-        <label htmlFor="password" className="grey-text">
-          Password
-        </label>
-        <input type="password" id="password" className="form-control" />
-        <div className="text-center mt-4">
-          <button className="btn btn-unique" type="submit">
-            Register
-          </button>
-        </div>
-      </form>
+      <div className="container form-group">
+        <form
+          className="needs-validation organisation-details"
+          onSubmit={this.onSubmit}
+        >
+          <p className="h4 text-center mb-4">
+            Please provide all the information requested. Your registration
+            cannot be completed without it.
+          </p>
+          <label htmlFor="formCompletionInformation" />
+
+          <div>
+            <label htmlFor="Name required" />
+            <input
+              type="text required value"
+              id="yourName"
+              placeholder="Your name"
+              className="form-control col-center"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="organisationName required" />
+            <input
+              type="text "
+              id="organisationName"
+              placeholder="Organisation name"
+              className="form-control col-center "
+              required
+            />
+            <div class="invalid-feedback">
+              Please provide your organisation name
+            </div>
+          </div>
+
+          <div>
+            <label htmlFor="address required" />
+            <input
+              type="text"
+              id="address"
+              placeholder="Address"
+              className="form-control col-center"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="email required" />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="form-control col-center"
+            />
+          </div>
+          <div>
+            <label htmlFor="phoneNumber required" />
+            <input
+              type="number"
+              id="number"
+              placeholder="Phone number"
+              className="form-control col-center"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="password required" />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="form-control col-center "
+            />
+          </div>
+          <div className="text-center mt-4">
+            <button className="btn btn-large btn-primary" type="submit">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     );
   }
 }
